aaf5858d9d756baa2e18499edd09c5abc0201eac
Angela-aa <44466647+Angela-aa@users.noreply.github.com>
2018-11-04 15:16:46 +0800

    Update main.js

--- main.js
+++ main.js
@@ -2,13 +2,23 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+import axios from 'axios'
+import'mint-ui/lib/style.css'
+Vue.prototype.axios = axios;
+axios.withCredentials = true;
 Vue.config.productionTip = false
 
-
+Vue.filter('Indexqiaomen',function(val){
+	return val==null?'我是一名国家儿童营养师，熟知婴幼儿健康饮食需求，精通厨艺，宝宝挑':val; 
+})
+axios.defaults.baseURL = 'http://127.0.0.1:3030'
 
 import VueResource from "vue-resource"
 Vue.use(VueResource)
+
+Vue.http.options.root="http://127.0.0.1:3030/"
+Vue.http.options.emulateJSON = true;
+
 new Vue({
   router,
   store,
